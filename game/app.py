ca83a49f4fe50d4de879784a32196cdd32731a4e
Sudhanva Hebbale <shebbale@asu.edu>
2021-03-10 11:51:40 -0700

    Initial COde

---
+++ game/app.py
@@ -0,0 +1,357 @@
+import datetime
+import os
+import pprint
+
+from tempfile import mkdtemp
+from flask import Flask, jsonify, request, render_template, url_for
+from flask_caching import Cache
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.exceptions import Forbidden
+from pylti1p3.contrib.flask import FlaskOIDCLogin, FlaskMessageLaunch, FlaskRequest, FlaskCacheDataStorage
+from pylti1p3.deep_link_resource import DeepLinkResource
+from pylti1p3.grade import Grade
+from pylti1p3.lineitem import LineItem
+from pylti1p3.tool_config import ToolConfJsonFile
+from pylti1p3.registration import Registration
+
+import requests
+
+
+'''
+FACE RECOGNITION
+'''
+# import face_recognition
+from flask import Flask, jsonify, request, redirect,render_template,Response
+import cv2
+import os
+from pathlib import Path
+import numpy as np
+from imutils.video import WebcamVideoStream
+from PIL import Image
+import sys
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
+app = Flask('pylti1p3-game-example', template_folder='templates', static_folder='static')
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+config = {
+    "DEBUG": True,
+    "ENV": "development",
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 600,
+    "SECRET_KEY": "replace-me",
+    "SESSION_TYPE": "filesystem",
+    "SESSION_FILE_DIR": mkdtemp(),
+    "SESSION_COOKIE_NAME": "flask-session-id",
+    "SESSION_COOKIE_HTTPONLY": True,
+    "SESSION_COOKIE_SECURE": False,   # should be True in case of HTTPS usage (production)
+    "SESSION_COOKIE_SAMESITE": None,  # should be 'None' in case of HTTPS usage (production)
+    "DEBUG_TB_INTERCEPT_REDIRECTS": False
+}
+app.config.from_mapping(config)
+cache = Cache(app)
+toolbar = DebugToolbarExtension(app)
+
+PAGE_TITLE = 'Game Example'
+
+
+class ExtendedFlaskMessageLaunch(FlaskMessageLaunch):
+
+    def validate_nonce(self):
+        """
+        Probably it is bug on "https://lti-ri.imsglobal.org":
+        site passes invalid "nonce" value during deep links launch.
+        Because of this in case of iss == http://imsglobal.org just skip nonce validation.
+
+        """
+        iss = self.get_iss()
+        deep_link_launch = self.is_deep_link_launch()
+        if iss == "http://imsglobal.org" and deep_link_launch:
+            return self
+        return super(ExtendedFlaskMessageLaunch, self).validate_nonce()
+
+
+def get_lti_config_path():
+    return os.path.join(app.root_path, '..', 'configs', 'game.json')
+
+
+def get_launch_data_storage():
+    return FlaskCacheDataStorage(cache)
+
+
+def get_jwk_from_public_key(key_name):
+    key_path = os.path.join(app.root_path, '..', 'configs', key_name)
+    f = open(key_path, 'r')
+    key_content = f.read()
+    jwk = Registration.get_jwk(key_content)
+    f.close()
+    return jwk
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    launch_data_storage = get_launch_data_storage()
+
+    flask_request = FlaskRequest()
+    target_link_uri = flask_request.get_param('target_link_uri')
+    if not target_link_uri:
+        raise Exception('Missing "target_link_uri" param')
+
+    oidc_login = FlaskOIDCLogin(flask_request, tool_conf, launch_data_storage=launch_data_storage)
+    return oidc_login\
+        .enable_check_cookies()\
+        .redirect(target_link_uri)
+
+
+@app.route('/launch/', methods=['POST'])
+def launch():
+    '''
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    flask_request = FlaskRequest()
+    launch_data_storage = get_launch_data_storage()
+    message_launch = ExtendedFlaskMessageLaunch(flask_request, tool_conf, launch_data_storage=launch_data_storage)
+    message_launch_data = message_launch.get_launch_data()
+    pprint.pprint(message_launch_data)
+
+    difficulty = message_launch_data.get('https://purl.imsglobal.org/spec/lti/claim/custom', {}) \
+        .get('difficulty', None)
+    if not difficulty:
+        difficulty = request.args.get('difficulty', 'normal')
+
+    tpl_kwargs = {
+        'page_title': PAGE_TITLE,
+        'is_deep_link_launch': message_launch.is_deep_link_launch(),
+        'launch_data': message_launch.get_launch_data(),
+        'launch_id': message_launch.get_launch_id(),
+        'curr_user_name': message_launch_data.get('name', ''),
+        'curr_diff': 'normal'
+    }
+    
+    return render_template('game.html', **tpl_kwargs)
+    '''
+    # return render_template('game.html')
+ 
+    return redirect('/predict')  
+
+@app.route('/jwks/', methods=['GET'])
+def get_jwks():
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    return jsonify({'keys': tool_conf.get_jwks()})
+
+
+@app.route('/configure/<launch_id>/<difficulty>/', methods=['GET', 'POST'])
+def configure(launch_id, difficulty):
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    flask_request = FlaskRequest()
+    launch_data_storage = get_launch_data_storage()
+    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
+                                                           launch_data_storage=launch_data_storage)
+
+    if not message_launch.is_deep_link_launch():
+        raise Forbidden('Must be a deep link!')
+
+    launch_url = url_for('launch', _external=True)
+
+    resource = DeepLinkResource()
+    resource.set_url(launch_url + '?difficulty=' + difficulty) \
+        .set_custom_params({'difficulty': difficulty}) \
+        .set_title('Breakout ' + difficulty + ' mode!')
+
+    html = message_launch.get_deep_link().output_response_form([resource])
+    return html
+
+
+@app.route('/api/score/<launch_id>/<earned_score>/<time_spent>/', methods=['POST'])
+def score(launch_id, earned_score, time_spent):
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    flask_request = FlaskRequest()
+    launch_data_storage = get_launch_data_storage()
+    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
+                                                           launch_data_storage=launch_data_storage)
+
+    resource_link_id = message_launch.get_launch_data() \
+        .get('https://purl.imsglobal.org/spec/lti/claim/resource_link', {}).get('id')
+
+    if not message_launch.has_ags():
+        raise Forbidden("Don't have grades!")
+
+    sub = message_launch.get_launch_data().get('sub')
+    timestamp = datetime.datetime.utcnow().isoformat() + 'Z'
+    earned_score = int(earned_score)
+    time_spent = int(time_spent)
+
+    grades = message_launch.get_ags()
+    sc = Grade()
+    sc.set_score_given(earned_score) \
+        .set_score_maximum(100) \
+        .set_timestamp(timestamp) \
+        .set_activity_progress('Completed') \
+        .set_grading_progress('FullyGraded') \
+        .set_user_id(sub)
+
+    sc_line_item = LineItem()
+    sc_line_item.set_tag('score') \
+        .set_score_maximum(100) \
+        .set_label('Score')
+    if resource_link_id:
+        sc_line_item.set_resource_id(resource_link_id)
+
+    grades.put_grade(sc, sc_line_item)
+
+    tm = Grade()
+    tm.set_score_given(time_spent) \
+        .set_score_maximum(999) \
+        .set_timestamp(timestamp) \
+        .set_activity_progress('Completed') \
+        .set_grading_progress('FullyGraded') \
+        .set_user_id(sub)
+
+    tm_line_item = LineItem()
+    tm_line_item.set_tag('time') \
+        .set_score_maximum(999) \
+        .set_label('Time Taken')
+    if resource_link_id:
+        tm_line_item.set_resource_id(resource_link_id)
+
+    result = grades.put_grade(tm, tm_line_item)
+
+    return jsonify({'success': True, 'result': result.get('body')})
+
+
+@app.route('/api/scoreboard/<launch_id>/', methods=['GET', 'POST'])
+def scoreboard(launch_id):
+    tool_conf = ToolConfJsonFile(get_lti_config_path())
+    flask_request = FlaskRequest()
+    launch_data_storage = get_launch_data_storage()
+    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
+                                                           launch_data_storage=launch_data_storage)
+
+    resource_link_id = message_launch.get_launch_data() \
+        .get('https://purl.imsglobal.org/spec/lti/claim/resource_link', {}).get('id')
+
+    if not message_launch.has_nrps():
+        raise Forbidden("Don't have names and roles!")
+
+    if not message_launch.has_ags():
+        raise Forbidden("Don't have grades!")
+
+    ags = message_launch.get_ags()
+
+    score_line_item = LineItem()
+    score_line_item.set_tag('score') \
+        .set_score_maximum(100) \
+        .set_label('Score')
+    if resource_link_id:
+        score_line_item.set_resource_id(resource_link_id)
+
+    scores = ags.get_grades(score_line_item)
+
+    time_line_item = LineItem()
+    time_line_item.set_tag('time') \
+        .set_score_maximum(999) \
+        .set_label('Time Taken')
+    if resource_link_id:
+        time_line_item.set_resource_id(resource_link_id)
+
+    times = ags.get_grades(time_line_item)
+
+    members = message_launch.get_nrps().get_members()
+    scoreboard_result = []
+
+    for sc in scores:
+        result = {'score': sc['resultScore']}
+        for tm in times:
+            if tm['userId'] == sc['userId']:
+                result['time'] = tm['resultScore']
+                break
+        for member in members:
+            if member['user_id'] == sc['userId']:
+                result['name'] = member.get('name', 'Unknown')
+                break
+        scoreboard_result.append(result)
+
+    return jsonify(scoreboard_result)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict_image():
+    
+    return '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Display Webcam Stream</title>
+    
+    </head>
+    
+    <body>
+    <div id="container">
+        <video autoplay="true" id="videoElement">
+        
+        </video>
+    </div>
+    <script>
+        var video = document.querySelector("#videoElement");
+        if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+            video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+            console.log("Something went wrong!");
+            });
+        }
+    </script>
+    </body>
+    </html>
+    '''    
+
+
+def video_capture():
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+    
+    ret, frame = video_capture.read()
+    
+    # frame_number += 1
+    return cv2.imencode('.png', frame)[1].tobytes()
+    
+def gen_frame():
+    """Video streaming generator function."""
+    while True:
+        frame = video_capture()
+        # stream = WebcamVideoStream(src=0).start()
+        # frame = stream.read()
+        # frame_ = cv2.imencode('.png', frame)[1].tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/png\r\n\r\n' + frame+ b'\r\n') # concate frame one by one and show result
+
+@app.route('/video_feed', methods=['GET', 'POST'])
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen_frame(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/test/', methods=['GET', 'POST'])
+def test():
+    response = requests.get("www.google.com")
+    print(response)
+    return render_template("game.html")
+
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=9001, threaded=True)
+    context = ('cert.pem', 'key.pem')
+    app.run(host='0.0.0.0', debug=True, threaded=True, ssl_context=context) 
